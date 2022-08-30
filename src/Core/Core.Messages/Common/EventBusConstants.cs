79f46ec880b0746993cd1e4ee5a2e7c9133f6721
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-08-30 18:58:35 +0300

    Core layer created and rabbitMq dockerized

---
+++ src/Core/Core.Messages/Common/EventBusConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace Core.Messages.Common
+{
+    public static class EventBusConstants
+	{
+		public const string BasketCheckoutQueue = "basketcheckout-queue";
+	}
+}
---
+++ src/Core/Core.Messages/Events/BaseEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Core.Messages.Events
+{
+    public class BaseEvent
+    {
+        public BaseEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public BaseEvent(Guid id, DateTime createDate)
+        {
+            Id = id;
+            CreationDate = createDate;
+        }
+
+        public Guid Id { get; private set; }
+
+        public DateTime CreationDate { get; private set; }
+    }
+}
