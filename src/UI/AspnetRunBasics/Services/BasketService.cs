0a3cc8dcf5514d40d423270844f819fd3e573b8f
veysel mutlu <veysel_mutlu42@hotmail.com>
2022-10-14 22:41:44 +0300

    uı configured deleted

---
+++ src/UI/AspnetRunBasics/Services/BasketService.cs
@@ -0,0 +1,44 @@
+﻿using AspnetRunBasics.Extensions;
+using AspnetRunBasics.Models;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"/Baskets/{userName}").ConfigureAwait(false);
+            return await response.ReadContentAs<BasketModel>().ConfigureAwait(false);
+        }
+
+        public async Task<BasketModel> UpdateBasket(BasketModel model)
+        {
+            var response = await _client.PostAsJson($"/Baskets", model).ConfigureAwait(false);
+            if (response.IsSuccessStatusCode)
+                return await response.ReadContentAs<BasketModel>().ConfigureAwait(false);
+            else
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
+        }
+
+        public async Task CheckoutBasket(BasketCheckoutModel model)
+        {
+            var response = await _client.PostAsJson($"/Baskets/Checkout", model).ConfigureAwait(false);
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Something went wrong when calling api.");
+            }
+        }
+    }
+}
